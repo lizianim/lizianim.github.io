ce66259cb2f6af44e32f88209490f341080a2ec2
Lisa Z <lisazieb@gmail.com>
2024-04-08 15:32:08 +0200

    automatedEmail

---
+++ automatedEmail/openIframe.js
@@ -0,0 +1,11 @@
+function openIframe(link) {
+    document.getElementById('contentIframe').src = link;
+}
+
+function openRightTopIframe(link) {
+    document.getElementById('rightTopIframe').src = link;
+}
+
+function openRightBottomIframe(link) {
+    document.getElementById('rightBottomIframe').src = link;
+}
